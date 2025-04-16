fedc46a1999deece19659483c4bf34b4d6201fab
Михаил <157118748+mikon051@users.noreply.github.com>
2025-04-16 09:53:48 +0300

    message

--- src/main/java/com/mystuff/mytgbot/MytgbotApplication.java
+++ src/main/java/com/mystuff/mytgbot/MytgbotApplication.java
@@ -1,14 +1,20 @@
 package com.mystuff.mytgbot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class MytgbotApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MytgbotApplication.class, args);
+    public static void main(String[] args) {
+        try {
+            String botToken = "7519567251:AAHGrYPsom0FJsgCYvyI3z53cvaOsWHfERQ";
+            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+            botsApplication.registerBot(botToken, new MyAmazingBot());
 
-	}
-
-
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
